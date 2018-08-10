b85682481f539f8624790491bbd63ac3c398a5f3
eduardogz94 <ezangarini@hotmail.com>
2018-08-10 02:17:13 -0400

    Proyect setup

--- utilities/loader.js
+++ utilities/loader.js
@@ -7,20 +7,19 @@ const logger = log4javascript.getDefaultLogger();
 // logger.error('testing logger from loader.js')
 // logger.fatal('testing logger from loader.js')
 
-
-ccc.addScript('components/renders/Login').then(response => {
+ccc.addScript('./renders/Login').then(response => {
     logger.info('added!')
 }).catch(err => {
     logger.error(err);
 })
 
-ccc.addScript('components/renders/Signup').then(response => {
+ccc.addScript('./renders/Signup').then(response => {
     logger.info('added!')
 }).catch(err => {
     logger.error(err);
 })
 
-ccc.addScript('components/renders/Themes').then(response => {
+ccc.addScript('./renders/Themes').then(response => {
     logger.info('added!')
 }).catch(err => {
     logger.error(err);
